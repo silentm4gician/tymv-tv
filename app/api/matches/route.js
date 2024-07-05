fe74e016087b1383fe7dfab0945bf164d6b4c778
Leandro Gonzalez Matkovich <leandrogonzalezmat@gmail.com>
2024-07-04 21:35:48 -0300

    routejs update

--- app/api/matches/route.js
+++ app/api/matches/route.js
@@ -1,6 +1,6 @@
 import { getMatches } from "../request";
 
-export const dynamic = 'force-dynamic';
+export const revalidate = 1;
 export async function GET() {
   try {
     const matches = await getMatches();
