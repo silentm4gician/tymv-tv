fa66aa08ec00c76768a9ee129344653ee073ac03
Leandro Gonzalez Matkovich <leandrogonzalezmat@gmail.com>
2024-07-23 17:13:40 -0300

    main page update

--- app/page.js
+++ app/page.js
@@ -24,11 +24,12 @@ export default async function Home() {
     <main className="flex flex-col items-center p-10 mt-10">
       <section>
         <h4 className="text-center text-xl mb-3 border-b rounded font-semibold">LISTA DE EVENTOS</h4>
-        {matches.length > 0 ? (
-          matches.map((match) => <MatchCard key={match.id} match={match} />)
+        {/* {matches.length > 0 ? (
+          matches?.map((match) => <MatchCard key={match.id} match={match} />)
         ) : (
           <p>Todavía no hay eventos</p>
-        )}
+        )} */}
+        {matches?.map((match) => <MatchCard key={match.id} match={match} />)}
       </section>
       <h1 className="text-2xl font-semibold px-16 py-4 bg-zinc-900 rounded-md mt-3 italic">
         TOCO Y ME VOY <span className="text-red-500 font-bold">TV</span>
